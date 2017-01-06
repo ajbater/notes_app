849baf8cccf1a348c8d66c5a89df8e8af95ce9b9
ajbater <amanda22@hotmail.co.uk>
2017-01-06 10:37:27 +0000

    add event listener to listen for hashchange

--- index.js
+++ index.js
@@ -2,7 +2,4 @@ noteList = new NoteList();
 noteController = new NoteController(noteList);
 noteController.createNote("Hello 12423423auwhreihehdjkbsjfsfjhsgdfjhsgd4342");
 noteController.getHtml();
-console.log(noteController.getNoteIdFromURL());
-console.log(note.id);
-console.log(parseInt(noteController.getNoteIdFromURL()));
-console.log(window.location.hash.split("#")[1]);
+noteController.makeNoteLinkShowFullNote();
